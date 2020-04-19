7e2be55c9a254aef66bcaf95e5f1b099ce53cc4b
Rishabh <rrishabh145@gmail.com>
2020-04-19 19:00:10 +0530

    Creating an Environment for our Object

--- blob_game.py
+++ blob_game.py
@@ -1,3 +1,17 @@
+import pygame
+import random
+
+WIDTH = 800
+HEIGHT = 600
+WHITE = (255,255,255)
+BLUE = (0,0,255)
+RED = (255,0,0)
+
+#initial game environment setup
+game_display = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("Blob World")
+clock = pygame.time.Clock()
+
 class Blob:
 	"""docstring for Blob"""
 	def __init__(self, color):
@@ -12,8 +26,30 @@ class Blob:
 		self.x += self.move_x
 		self.y += self.move_y
 
+		#to prevent the blob from running off the creen
 		if self.x < 0: self.x = 0
 		elif self.x > WIDTH: self.x = WIDTH
 
 		if self.y<0: self.y = 0
 		elif self.y > HEIGHT: self.y = HEIGHT
+
+
+def draw_environment(blob):
+	game_display.fill(WHITE) # clear and redraw after very frame
+	pygame.draw.circle(game_display,blob.color, [blob.x, blob.y], blob.size) # circle( where to display, what to display)
+	pygame.display.update() # this step brings the previous step in action
+	blob.move()
+
+def main():
+	red_blob = Blob(color=RED)
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT: # if the game is quit by user
+				pygame.quit()
+				quit()
+
+		draw_environment(red_blob)
+		clock.tick(60) #to limit FPS
+
+if __name__ == '__main__':
+	main()
